c6de9f72b86a2a7dc6d2d4b9510edef0d192e184
Ravi <RaviOP@users.noreply.github.com>
2020-12-07 17:27:22 +0530

    Init Commit

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('./db/mongoose')
 const userRoutes = require('./routes/user')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
 app.use(userRoutes)
 
